f604da5226e41390526b84101f11ef009ea52716
Sree Gajula <gajulas@oregonstate.edu>
2021-05-31 19:22:18 -0700

    leap year code

--- test_leapyear.py
+++ test_leapyear.py
@@ -1,8 +1,8 @@
 import unittest
 from leapyear import check_leap
 
-class TestFizz(unittest.TestCase):
-    def test_fizzbuzz(self):
+class TestLeap(unittest.TestCase):
+    def test_leap(self):
         self.assertEqual(check_leap(2020), 1)
-        self.assertEqual(check_leap(2030), 0)
+        self.assertEqual(check_leap(2031), 1)
         self.assertEqual(check_leap(2012), 1)

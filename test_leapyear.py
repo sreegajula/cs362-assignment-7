25463322e8b70c26f8841b3aa655b46a57a13fe1
Sree Gajula <gajulas@oregonstate.edu>
2021-05-31 19:25:16 -0700

    second leap year test: tests not leap only

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,5 +4,11 @@ from leapyear import check_leap
 class TestLeap(unittest.TestCase):
     def test_leap(self):
         self.assertEqual(check_leap(2020), 1)
-        self.assertEqual(check_leap(2031), 1)
+        self.assertEqual(check_leap(2032), 1)
         self.assertEqual(check_leap(2012), 1)
+
+class TestNotLeap(unittest.TestCase):
+    def test_leap(self):
+        self.assertEqual(check_leap(2019), 0)
+        self.assertEqual(check_leap(2031), 0)
+        self.assertEqual(check_leap(2011), 0)

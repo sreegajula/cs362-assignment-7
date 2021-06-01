7689d1312bcd949749aa48e019a3a414fe2e549e
Sree Gajula <gajulas@oregonstate.edu>
2021-05-31 19:14:13 -0700

    second test: tests for BUZZ

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,11 +2,19 @@ import unittest
 from fizzbuzz import test_buzz
 
 
-class TestFizzBuzz(unittest.TestCase):
+class TestFizz(unittest.TestCase):
     def test_fizzbuzz(self):
         self.assertEqual(test_buzz(3), "Fizz")
         self.assertEqual(test_buzz(9), "Fizz")
         self.assertEqual(test_buzz(12), "Fizz")
 
+class TestBuzz(unittest.TestCase):
+    def test_fizzbuzz(self):
+        self.assertEqual(test_buzz(5), "Buzz")
+        self.assertEqual(test_buzz(10), "Buzz")
+        self.assertEqual(test_buzz(20), "Buzz")
+
+
 if __name__ == '__main__':
     unittest.main()
+
